5ac9d2585cf25aaad37c704479f909ba30f50ca4
devchanki <kgichan12@gmail.com>
2024-08-05 07:38:36 +0900

    엑셀 만드는 반복되는 로직 제거

--- src/main/java/org/chanki/ExcelManager.java
+++ src/main/java/org/chanki/ExcelManager.java
@@ -12,27 +12,54 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelManager {
 
-  public void convertJsonToExcel(JsonNode jsonNode, List<String> keyList, String fileName)
-      throws IOException {
+  public void saveWorkBook(List<List<Object>> rowItemList, List<String> headerItemList, String fileName) throws IOException {
     Workbook workbook = new XSSFWorkbook();
     Sheet sheet = workbook.createSheet("data");
 
     // 주어진 리스트로 헤더 생성
     Row headerRow = sheet.createRow(0);
     int cellNum = 0;
-    for (String header : keyList) {
+    for (String header : headerItemList) {
       Cell cell = headerRow.createCell(cellNum++);
       cell.setCellValue(header);
     }
 
     int rowNum = 1;
-
-    for (JsonNode node : jsonNode) {
+    for (List<Object> rowItem : rowItemList) {
       Row row = sheet.createRow(rowNum++);
       int _cellNum = 0;
-      for(String key: keyList) {
+      for(var key: rowItem) {
         Cell cell = row.createCell(_cellNum++);
-        cell.setCellValue(String.valueOf(node.get(key)));
+        cell.setCellValue(key.toString());
+      }
+    }
+
+    // 엑셀 파일로 저장
+    try (
+        FileOutputStream fileOut = new FileOutputStream(fileName)) {
+      workbook.write(fileOut);
+    } workbook.close();
+  }
+
+  public void saveWorkBookWithJsonNode(JsonNode jsonNode, List<String> headerItemList, String fileName) throws IOException {
+    Workbook workbook = new XSSFWorkbook();
+    Sheet sheet = workbook.createSheet("data");
+
+    // 주어진 리스트로 헤더 생성
+    Row headerRow = sheet.createRow(0);
+    int cellNum = 0;
+    for (String header : headerItemList) {
+      Cell cell = headerRow.createCell(cellNum++);
+      cell.setCellValue(header);
+    }
+
+    int rowNum = 1;
+    for (JsonNode rowItem : jsonNode) {
+      Row row = sheet.createRow(rowNum++);
+      int _cellNum = 0;
+      for(String key: headerItemList) {
+        Cell cell = row.createCell(_cellNum++);
+        cell.setCellValue(rowItem.get(key).toString());
       }
     }
 
--- src/main/java/org/chanki/Json2Sheet.java
+++ src/main/java/org/chanki/Json2Sheet.java
@@ -3,7 +3,6 @@ package org.chanki;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -52,32 +51,8 @@ public class Json2Sheet {
 
     JsonNode jsonNode = jsonParser.getRootNode();
 
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("data");
-
-    // 주어진 리스트로 헤더 생성
-    Row headerRow = sheet.createRow(0);
-    int cellNum = 0;
-    for (String header : keyList) {
-      Cell cell = headerRow.createCell(cellNum++);
-      cell.setCellValue(header);
-    }
-
-    int rowNum = 1;
-
-    for (JsonNode node : jsonNode) {
-      Row row = sheet.createRow(rowNum++);
-      int _cellNum = 0;
-      for(String key: keyList) {
-        Cell cell = row.createCell(_cellNum++);
-        cell.setCellValue(String.valueOf(node.get(key)));
-      }
-    }
-    // 엑셀 파일로 저장
-    try (
-        FileOutputStream fileOut = new FileOutputStream(filePath)) {
-      workbook.write(fileOut);
-    } workbook.close();
+    ExcelManager excelManager = new ExcelManager();
+    excelManager.saveWorkBookWithJsonNode(jsonNode, keyList, filePath);
   }
 
   // Header 가 될 키를 전체 Json 순회 후 Key를 계산 후 Excel 헤더 만듬.
@@ -87,66 +62,17 @@ public class Json2Sheet {
     JsonNode jsonNode = jsonParser.getRootNode();
     List<String> keyList = jsonParser.getJsonObjectKeyList();
 
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("data");
-
-    // 주어진 리스트로 헤더 생성
-    Row headerRow = sheet.createRow(0);
-    int cellNum = 0;
-    for (String header : keyList) {
-      Cell cell = headerRow.createCell(cellNum++);
-      cell.setCellValue(header);
-    }
-
-    int rowNum = 1;
-
-    for (JsonNode node : jsonNode) {
-      Row row = sheet.createRow(rowNum++);
-      int _cellNum = 0;
-      for(String key: keyList) {
-        Cell cell = row.createCell(_cellNum++);
-        cell.setCellValue(String.valueOf(node.get(key)));
-      }
-    }
-    // 엑셀 파일로 저장
-    try (
-        FileOutputStream fileOut = new FileOutputStream(filePath)) {
-      workbook.write(fileOut);
-    } workbook.close();
+    ExcelManager excelManager = new ExcelManager();
+    excelManager.saveWorkBookWithJsonNode(jsonNode, keyList, filePath);
   }
 
   public void convertJsonToExcelToFilePath(String jsonPath, String filePath) throws Exception{
     JsonParser jsonParser = new JsonParser().builder().path(jsonPath).build().initialize();
-
     JsonNode jsonNode = jsonParser.getRootNode();
     List<String> keyList = jsonParser.getJsonObjectKeyList();
 
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("data");
-
-    // 주어진 리스트로 헤더 생성
-    Row headerRow = sheet.createRow(0);
-    int cellNum = 0;
-    for (String header : keyList) {
-      Cell cell = headerRow.createCell(cellNum++);
-      cell.setCellValue(header);
-    }
-
-    int rowNum = 1;
-
-    for (JsonNode node : jsonNode) {
-      Row row = sheet.createRow(rowNum++);
-      int _cellNum = 0;
-      for(String key: keyList) {
-        Cell cell = row.createCell(_cellNum++);
-        cell.setCellValue(String.valueOf(node.get(key)));
-      }
-    }
-    // 엑셀 파일로 저장
-    try (
-        FileOutputStream fileOut = new FileOutputStream(filePath)) {
-      workbook.write(fileOut);
-    } workbook.close();
+    ExcelManager excelManager = new ExcelManager();
+    excelManager.saveWorkBookWithJsonNode(jsonNode, keyList, filePath);
   }
 
   public void convertJsonToExcelToFilePath(InputStream stream, String filePath) throws Exception{
@@ -154,32 +80,9 @@ public class Json2Sheet {
     JsonNode jsonNode = jsonParser.getRootNode();
     List<String> keyList = jsonParser.getJsonObjectKeyList();
 
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("data");
+    ExcelManager excelManager = new ExcelManager();
+    excelManager.saveWorkBookWithJsonNode(jsonNode, keyList, filePath);
 
-    // 주어진 리스트로 헤더 생성
-    Row headerRow = sheet.createRow(0);
-    int cellNum = 0;
-    for (String header : keyList) {
-      Cell cell = headerRow.createCell(cellNum++);
-      cell.setCellValue(header);
-    }
-
-    int rowNum = 1;
-
-    for (JsonNode node : jsonNode) {
-      Row row = sheet.createRow(rowNum++);
-      int _cellNum = 0;
-      for(String key: keyList) {
-        Cell cell = row.createCell(_cellNum++);
-        cell.setCellValue(String.valueOf(node.get(key)));
-      }
-    }
-    // 엑셀 파일로 저장
-    try (
-        FileOutputStream fileOut = new FileOutputStream(filePath)) {
-      workbook.write(fileOut);
-    } workbook.close();
   }
 
   public void convertHttpResponseJsonToExcelToFilePath(String url, String filePath) throws Exception{
@@ -187,94 +90,20 @@ public class Json2Sheet {
     JsonNode jsonNode = jsonParser.getRootNode();
     List<String> keyList = jsonParser.getJsonObjectKeyList();
 
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("data");
-
-    // 주어진 리스트로 헤더 생성
-    Row headerRow = sheet.createRow(0);
-    int cellNum = 0;
-    for (String header : keyList) {
-      Cell cell = headerRow.createCell(cellNum++);
-      cell.setCellValue(header);
-    }
-
-    int rowNum = 1;
-
-    for (JsonNode node : jsonNode) {
-      Row row = sheet.createRow(rowNum++);
-      int _cellNum = 0;
-      for(String key: keyList) {
-        Cell cell = row.createCell(_cellNum++);
-        cell.setCellValue(String.valueOf(node.get(key)));
-      }
-    }
-    // 엑셀 파일로 저장
-    try (
-        FileOutputStream fileOut = new FileOutputStream(filePath)) {
-      workbook.write(fileOut);
-    } workbook.close();
+    ExcelManager excelManager = new ExcelManager();
+    excelManager.saveWorkBookWithJsonNode(jsonNode, keyList, filePath);
   }
 
   public void convertJsonToExcel(JsonNode jsonNode, List<String> keyList, String fileName)
       throws IOException {
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("data");
-
-    // 주어진 리스트로 헤더 생성
-    Row headerRow = sheet.createRow(0);
-    int cellNum = 0;
-    for (String header : keyList) {
-      Cell cell = headerRow.createCell(cellNum++);
-      cell.setCellValue(header);
-    }
-
-    int rowNum = 1;
-
-    for (JsonNode node : jsonNode) {
-      Row row = sheet.createRow(rowNum++);
-      int _cellNum = 0;
-      for(String key: keyList) {
-        Cell cell = row.createCell(_cellNum++);
-        cell.setCellValue(String.valueOf(node.get(key)));
-      }
-    }
-
-    // 엑셀 파일로 저장
-    try (
-        FileOutputStream fileOut = new FileOutputStream(fileName)) {
-      workbook.write(fileOut);
-    } workbook.close();
+    ExcelManager excelManager = new ExcelManager();
+    excelManager.saveWorkBookWithJsonNode(jsonNode, keyList, fileName);
   }
 
   public void convertJsonFileToExcel(JsonNode jsonNode, List<String> keyList, String fileName)
       throws IOException {
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("data");
-
-    // 주어진 리스트로 헤더 생성
-    Row headerRow = sheet.createRow(0);
-    int cellNum = 0;
-    for (String header : keyList) {
-      Cell cell = headerRow.createCell(cellNum++);
-      cell.setCellValue(header);
-    }
-
-    int rowNum = 1;
-
-    for (JsonNode node : jsonNode) {
-      Row row = sheet.createRow(rowNum++);
-      int _cellNum = 0;
-      for(String key: keyList) {
-        Cell cell = row.createCell(_cellNum++);
-        cell.setCellValue(String.valueOf(node.get(key)));
-      }
-    }
-
-    // 엑셀 파일로 저장
-    try (
-        FileOutputStream fileOut = new FileOutputStream(fileName)) {
-      workbook.write(fileOut);
-    } workbook.close();
+    ExcelManager excelManager = new ExcelManager();
+    excelManager.saveWorkBookWithJsonNode(jsonNode, keyList, fileName);
   }
 
 }
